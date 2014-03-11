2de55f735be1ba6a98a151ee3e1bea2b643e3da6
Max Fierke <max@maxfierke.com>
2014-03-11 17:12:37 -0500

    Distinguish between Residence Phones and Person Phones
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/UMRA/Bundle/MemberBundle/Controller/PhoneController.php
+++ src/UMRA/Bundle/MemberBundle/Controller/PhoneController.php
@@ -35,18 +35,35 @@ class PhoneController extends Controller
      *
      * @Template("UMRAMemberBundle:Phone:new.html.twig")
      */
-    public function createAction(Request $request)
+    public function createAction(Request $request, $personId = null, $residenceId = null)
     {
         $entity = new Phone();
-        $form = $this->createCreateForm($entity);
+        $form = $this->createCreateForm($entity, $personId, $residenceId);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+
+            if($personId) {
+                $person = $em->getRepository('UMRAMemberBundle:Person')->find($personId);
+                if (!$person) {
+                    throw $this->createNotFoundException();
+                }
+                $entity->setPerson($person);
+                $hid = $person->getHousehold()->getId();
+            } elseif ($residenceId) {
+                $residence = $em->getRepository('UMRAMemberBundle:Residence')->find($residenceId);
+                if (!$residence) {
+                    throw $this->createNotFoundException();
+                }
+                $entity->setResidence($residence);
+                $hid = $residence->getHousehold()->getId();
+            }
+
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('UMRA_Phone_show', array('id' => $entity->getId())));
+            return $this->redirect($this->generateUrl('UMRA_Household_show', array('id' => $hid)));
         }
 
         return array(
@@ -62,14 +79,23 @@ class PhoneController extends Controller
     *
     * @return \Symfony\Component\Form\Form The form
     */
-    private function createCreateForm(Phone $entity)
+    private function createCreateForm(Phone $entity, $personId = null, $residenceId = null)
     {
-        $form = $this->createForm(new PhoneType(), $entity, array(
-            'action' => $this->generateUrl('UMRA_Phone_create'),
-            'method' => 'POST',
-        ));
+        if ($residenceId != null && $personId === null) {
+            $entity->setPhtype('shared');
+            $form = $this->createForm(new PhoneType(), $entity, array(
+                'action' => $this->generateUrl('UMRA_Residence_Phone_create', array('residenceId' => $residenceId)),
+                'method' => 'POST',
+            ));
+            $form->remove('phtype');
+        } else {
+            $form = $this->createForm(new PhoneType(), $entity, array(
+                'action' => $this->generateUrl('UMRA_Person_Phone_create', array('personId' => $personId)),
+                'method' => 'POST',
+            ));
+        }
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+        $form->add('submit', 'submit', array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary')));
 
         return $form;
     }
@@ -79,14 +105,31 @@ class PhoneController extends Controller
      *
      * @Template()
      */
-    public function newAction()
+    public function newAction($personId = null, $residenceId = null)
     {
         $entity = new Phone();
-        $form   = $this->createCreateForm($entity);
+        $form   = $this->createCreateForm($entity, $personId, $residenceId);
+
+        $em = $this->getDoctrine()->getManager();
+
+        if ($personId) {
+            $person = $em->getRepository('UMRAMemberBundle:Person')->find($personId);
+            if (!$person) {
+                throw $this->createNotFoundException();
+            }
+            $householdId = $person->getHousehold()->getId();
+        } else {
+            $residence = $em->getRepository('UMRAMemberBundle:Residence')->find($residenceId);
+            if (!$residence) {
+                throw $this->createNotFoundException();
+            }
+            $householdId = $residence->getHousehold()->getId();
+        }
 
         return array(
             'entity' => $entity,
             'form'   => $form->createView(),
+            'householdId' => $householdId,
         );
     }
 
@@ -105,11 +148,18 @@ class PhoneController extends Controller
             throw $this->createNotFoundException('Unable to find Phone entity.');
         }
 
+        if ($entity->getPerson()) {
+            $householdId = $entity->getPerson()->getHousehold()->getId();
+        } else {
+            $householdId = $entity->getResidence()->getHousehold()->getId();
+        }
+
         $deleteForm = $this->createDeleteForm($id);
 
         return array(
             'entity'      => $entity,
             'delete_form' => $deleteForm->createView(),
+            'householdId' => $householdId,
         );
     }
 
@@ -128,13 +178,21 @@ class PhoneController extends Controller
             throw $this->createNotFoundException('Unable to find Phone entity.');
         }
 
-        $editForm = $this->createEditForm($entity);
+        if ($entity->getPerson()) {
+            $householdId = $entity->getPerson()->getHousehold()->getId();
+        } else {
+            $householdId = $entity->getResidence()->getHousehold()->getId();
+            $residenceId = $entity->getResidence()->getId();
+        }
+
+        $editForm = $this->createEditForm($entity, $residenceId);
         $deleteForm = $this->createDeleteForm($id);
 
         return array(
             'entity'      => $entity,
             'edit_form'   => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
+            'householdId' => $householdId,
         );
     }
 
@@ -145,14 +203,18 @@ class PhoneController extends Controller
     *
     * @return \Symfony\Component\Form\Form The form
     */
-    private function createEditForm(Phone $entity)
+    private function createEditForm(Phone $entity, $residenceId = null)
     {
         $form = $this->createForm(new PhoneType(), $entity, array(
             'action' => $this->generateUrl('UMRA_Phone_update', array('id' => $entity->getId())),
             'method' => 'PUT',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Update'));
+        if ($residenceId != null && $entity->getPerson() == null) {
+            $form->remove('phtype');
+        }
+
+        $form->add('submit', 'submit', array('label' => 'Update', 'attr' => array('class' => 'btn btn-primary')));
 
         return $form;
     }
@@ -171,20 +233,28 @@ class PhoneController extends Controller
             throw $this->createNotFoundException('Unable to find Phone entity.');
         }
 
+        if ($entity->getPerson()) {
+            $householdId = $entity->getPerson()->getHousehold()->getId();
+        } else {
+            $householdId = $entity->getResidence()->getHousehold()->getId();
+            $residenceId = $entity->getResidence()->getId();
+        }
+
         $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
+        $editForm = $this->createEditForm($entity, $residenceId);
         $editForm->handleRequest($request);
 
         if ($editForm->isValid()) {
             $em->flush();
 
-            return $this->redirect($this->generateUrl('UMRA_Phone_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('UMRA_Household_show', array('id' => $householdId)));
         }
 
         return array(
             'entity'      => $entity,
             'edit_form'   => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
+            'householdId' => $householdId,
         );
     }
     /**
@@ -196,19 +266,26 @@ class PhoneController extends Controller
         $form = $this->createDeleteForm($id);
         $form->handleRequest($request);
 
+        $entity = $em->getRepository('UMRAMemberBundle:Phone')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Phone entity.');
+        }
+
+        if ($entity->getPerson()) {
+            $householdId = $entity->getPerson()->getHousehold()->getId();
+        } else {
+            $householdId = $entity->getResidence()->getHousehold()->getId();
+        }
+
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('UMRAMemberBundle:Phone')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Phone entity.');
-            }
 
             $em->remove($entity);
             $em->flush();
         }
 
-        return $this->redirect($this->generateUrl('UMRA_Phone'));
+        return $this->redirect($this->generateUrl('UMRA_Household_show', array('id' => $householdId)));
     }
 
     /**
@@ -223,7 +300,7 @@ class PhoneController extends Controller
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('UMRA_Phone_delete', array('id' => $id)))
             ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->add('submit', 'submit', array('label' => 'Delete', 'attr' => array('class' => 'btn btn-danger')))
             ->getForm()
         ;
     }
