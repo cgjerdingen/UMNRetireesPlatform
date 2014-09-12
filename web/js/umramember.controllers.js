06515238b636af86887f20c35d40eb84cc6a2568
Max Fierke <max@maxfierke.com>
2014-09-12 12:32:06 -0500

    Redo the registration form in more "pure" angular.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- web/js/umramember.controllers.js
+++ web/js/umramember.controllers.js
@@ -6,6 +6,17 @@ angular.module('umramember.controllers', [])
     .controller('RegistrationCtrl', ['$scope', function($scope) {
         $scope.memberStatus = 'new';
 
+        $scope.household = {
+            postalname: '',
+            members: [
+                {
+                    firstname: '',
+                    lastname: ''
+                }
+            ],
+            residences: []
+        };
+
         $scope.counts = {
             members: 1,
             parkingCoupons: 1
@@ -15,6 +26,16 @@ angular.module('umramember.controllers', [])
             luncheon: 0,
             total: 0
         };
+
+        $scope.$watch(function () { return $scope.household.members[0].firstname + " " + $scope.household.members[0].lastname; }, function (newVal, oldVal) {
+            if ($scope.household.postalname === oldVal || newVal === oldVal) {
+                $scope.household.postalname = newVal;
+            }
+
+            if ($scope.household.members[0].fullname === oldVal || newVal === oldVal) {
+                $scope.household.members[0].fullname = newVal;
+            }
+        });
 
         $scope.$watch(function () { return parseInt($scope.costs.member) + parseInt($scope.costs.luncheon) }, function (newVal, oldVal) {
             $scope.costs.total = newVal;
