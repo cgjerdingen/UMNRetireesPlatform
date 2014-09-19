226d1e9eb95dd7a34f117da16c24c126bd0ee118
Max Fierke <max@maxfierke.com>
2014-09-18 21:43:33 -0500

    Section out MemberAdmin
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/UMRA/Bundle/MemberBundle/Admin/MemberAdmin.php
+++ src/UMRA/Bundle/MemberBundle/Admin/MemberAdmin.php
@@ -16,85 +16,96 @@ class MemberAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-           ->add('lastname', 'text', array(
-                    'label' => 'Last Name'
-                ))
-            ->add('firstname', 'text', array(
-                    'label' => 'First Name'
-                ))
-            ->add('nickname', 'text', array(
-                    'label' => 'Nickname'
-                ))
-            ->add('fullname', 'text', array(
-                    'label' => 'Full Name'
-                ))
-            ->add('nametagname', 'text', array(
-                    'label' => 'Nametag Name'
-                ))
-            ->add('membersince', 'date', array(
-                    'years' => range(1910, date('Y')),
-                    'label' => 'UMRA Member Since',
-                    'required' => false
-                ))
-            ->add('utopunit', 'text', array(
-                    'label' => 'University Unit',
-                    'required' => false
-                ))
-            ->add('udeptequiv', 'text', array(
-                    'label' => 'University Department',
-                    'required' => false
-                ))
-            ->add('uempltype', 'choice', array(
-                    'choices' => array(
-                        'faculty' => 'Faculty',
-                        'acad_prof' => 'Academic Professional',
-                        'acad_adm' => 'Academic Administrator',
-                        'civ_srv' => 'Civil Service',
-                        'afscme_cler' => 'AFSCME Clerical',
-                        'afscme_tech' => 'AFSCME Technical',
-                        'teamster' => 'Teamster'
-                    ),
-                    'label' => 'Employment Type',
-                    'required' => false
-                ))
-            ->add('utitle', 'text', array(
-                    'label' => 'Title',
-                    'required' => false
-                ))
-            ->add('ustartdate', 'date', array(
-                    'years' => range(1910, date('Y')),
-                    'label' => 'University Start Date',
-                    'required' => false
-                ))
-            ->add('uretiredate', 'date', array(
-                    'years' => range(1910, date('Y')),
-                    'label' => 'University Retire Date',
-                    'required' => false
-                ))
-            ->add('deceasedate', 'date', array(
-                    'years' => range(1910, date('Y')),
-                    'label' => 'Deceased Date',
-                    'required' => false
-                ))
-            ->add('activenow', 'checkbox', array(
-                    'label' => 'Active',
-                    'required' => false
-                ))
-            ->add('newsPref', 'choice', array(
-                    'choices' => array(
-                        'postal' => 'Postal Mail',
-                        'email' => 'Email',
-                        'both' => 'Both'
-                    ),
-                    'label' => 'Newsletter Preference',
-                    'expanded' => true,
-                    'required' => false
-                ))
-            ->add('weburl', 'url', array(
-                    'label' => 'Personal Website',
-                    'required' => false,
-                    'attr' => array('placeholder' => 'e.g. http://umn.edu/~test001/')
-                ))
+            ->with('Personal Information')
+                ->add('household')
+                ->add('lastname', 'text', array(
+                        'label' => 'Last Name'
+                    ))
+                ->add('firstname', 'text', array(
+                        'label' => 'First Name'
+                    ))
+                ->add('nickname', 'text', array(
+                        'label' => 'Nickname'
+                    ))
+                ->add('fullname', 'text', array(
+                        'label' => 'Full Name'
+                    ))
+                ->add('nametagname', 'text', array(
+                        'label' => 'Nametag Name'
+                    ))
+                ->add('deceasedate', 'date', array(
+                        'years' => range(1910, date('Y')),
+                        'label' => 'Deceased Date',
+                        'required' => false
+                    ))
+                ->add('weburl', 'url', array(
+                        'label' => 'Personal Website',
+                        'required' => false,
+                        'attr' => array('placeholder' => 'e.g. http://umn.edu/~test001/')
+                    ))
+            ->end()
+            ->with('University Information')
+                ->add('utopunit', 'text', array(
+                        'label' => 'University Unit',
+                        'required' => false
+                    ))
+                ->add('udeptequiv', 'text', array(
+                        'label' => 'University Department',
+                        'required' => false
+                    ))
+                ->add('uempltype', 'choice', array(
+                        'choices' => array(
+                            'faculty' => 'Faculty',
+                            'acad_prof' => 'Academic Professional',
+                            'acad_adm' => 'Academic Administrator',
+                            'civ_srv' => 'Civil Service',
+                            'afscme_cler' => 'AFSCME Clerical',
+                            'afscme_tech' => 'AFSCME Technical',
+                            'teamster' => 'Teamster'
+                        ),
+                        'label' => 'Employment Type',
+                        'required' => false
+                    ))
+                ->add('utitle', 'text', array(
+                        'label' => 'Title',
+                        'required' => false
+                    ))
+                ->add('ustartdate', 'date', array(
+                        'years' => range(1910, date('Y')),
+                        'label' => 'University Start Date',
+                        'required' => false
+                    ))
+                ->add('uretiredate', 'date', array(
+                        'years' => range(1910, date('Y')),
+                        'label' => 'University Retire Date',
+                        'required' => false
+                    ))
+            ->end()
+            ->with('UMRA Information')
+                ->add('membersince', 'date', array(
+                        'years' => range(1910, date('Y')),
+                        'label' => 'UMRA Member Since',
+                        'required' => false
+                    ))
+                ->add('activenow', 'checkbox', array(
+                        'label' => 'Active',
+                        'required' => false
+                    ))
+                ->add('newsPref', 'choice', array(
+                        'choices' => array(
+                            'postal' => 'Postal Mail',
+                            'email' => 'Email',
+                            'both' => 'Both'
+                        ),
+                        'label' => 'Newsletter Preference',
+                        'expanded' => false,
+                        'required' => false
+                    ))
+            ->end()
+            ->with('Emails')
+                ->add('emailCanonical', 'email', array('label' => 'Login Email Address'))
+                ->add('emails')
+            ->end()
         ;
     }
 
