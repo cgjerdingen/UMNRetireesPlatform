63ca2da6105c699194ae41509f2ca4949aa1d29c
Max Fierke <max@maxfierke.com>
2014-10-16 22:42:33 -0500

    Add sonata admin section for transactions.
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ src/UMRA/Bundle/MemberBundle/Admin/TransAdmin.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace UMRA\Bundle\MemberBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class TransAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('trandate', null, array('label' => 'Transaction Date'))
+            ->add('trantype', null, array('label' => 'Transaction Type'))
+            ->add('amount')
+            ->add('pmtmethod', null, array('label' => 'Payment Method'))
+            ->add('servicechg', null, array('label' => 'Service Charge'))
+            ->add('doneby', null, array('label' => 'Done By'))
+            ->add('person')
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('trandate', null, array('label' => 'Transaction Date'))
+            ->add('trantype', null, array('label' => 'Transaction Type'))
+            ->add('amount')
+            ->add('pmtmethod', null, array('label' => 'Payment Method'))
+            ->add('servicechg', null, array('label' => 'Service Charge'))
+            ->add('doneby', null, array('label' => 'Done By'))
+            ->add('person')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('trandate', null, array('label' => 'Transaction Date'))
+            ->add('trantype', 'choice', array(
+                'label' => 'Transaction Type',
+                'choices' => array('MEMBERSHIP_NEW' => 'New Membership', 'MEMBERSHIP_RENEW' => 'Membership Renewal', 'LUNCHEON_FEE' => 'Luncheon Fee', 'OTHER' => 'Other'),
+                ))
+            ->add('amount')
+            ->add('pmtmethod', 'choice', array(
+                'label' => 'Payment Method',
+                'choices' => array('CREDIT_CARD' => 'Credit Card', 'CHECK' => 'Check', 'OTHER' => 'Other'),
+                ))
+            ->add('servicechg', null, array('label' => 'Service Charge'))
+            ->add('doneby', null, array('label' => 'Done By'))
+            ->add('notes')
+            ->add('person')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('trandate', null, array('label' => 'Transaction Date'))
+            ->add('trantype', null, array('label' => 'Transaction Type'))
+            ->add('amount')
+            ->add('pmtmethod', null, array('label' => 'Payment Method'))
+            ->add('servicechg', null, array('label' => 'Service Charge'))
+            ->add('doneby', null, array('label' => 'Done By'))
+            ->add('person')
+            ->add('notes')
+            ->add('id')
+        ;
+    }
+}
