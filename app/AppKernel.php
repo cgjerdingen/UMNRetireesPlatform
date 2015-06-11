e1f556b5066361810500b68818786159fea528b9
Max Fierke <max@maxfierke.com>
2015-06-10 22:02:26 -0500

    Add LexikFormFilterBundle
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
