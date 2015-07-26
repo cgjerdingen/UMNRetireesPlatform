bb01ebb2dea3de258a2054ccf54b71c59d2a8907
Max Fierke <max@maxfierke.com>
2015-07-26 13:32:55 -0500

    Cleanup RegistrationCtrl form integration
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- web/js/umramember.controllers.js
+++ web/js/umramember.controllers.js
@@ -1,43 +1,56 @@
-'use strict';
+(function () {
+    'use strict';
+    angular.module('umramember.controllers', [])
+        .controller('RegistrationCtrl', ['$scope', function($scope) {
+            $scope.household = {
+                postalname: '',
+                members: [
+                    {
+                        firstname: '',
+                        lastname: '',
+                        fullname: '',
+                        nametagname: '',
+                        nickname: ''
+                    }
+                ],
+                residences: []
+            };
 
-/* Controllers */
+            $scope.types = {
+                luncheon: null
+            };
 
-angular.module('umramember.controllers', [])
-    .controller('RegistrationCtrl', ['$scope', function($scope) {
-        $scope.memberStatus = 'new';
+            $scope.counts = {
+                members: 1,
+                parkingCoupons: 0
+            };
+            $scope.costs = {
+                member: 0,
+                luncheon: 0
+            };
 
-        $scope.household = {
-            postalname: '',
-            members: [
-                {
-                    firstname: '',
-                    lastname: ''
+            $scope.getTotalCost = function () {
+                return parseInt($scope.costs.member) + parseInt($scope.costs.luncheon);
+            };
+
+            $scope.$watch(function () {
+              return (($scope.household.members[0].firstname || "") + " " + ($scope.household.members[0].lastname || "")).trim();
+            }, function (newVal, oldVal) {
+                if ($scope.household.postalname === oldVal || newVal === oldVal) {
+                    $scope.household.postalname = newVal;
                 }
-            ],
-            residences: []
-        };
 
-        $scope.counts = {
-            members: 1,
-            parkingCoupons: 1
-        };
-        $scope.costs = {
-            member: 0,
-            luncheon: 0,
-            total: 0
-        };
+                if ($scope.household.members[0].nametagname === oldVal || newVal === oldVal) {
+                    $scope.household.members[0].nametagname = newVal || "";
+                }
 
-        $scope.$watch(function () { return $scope.household.members[0].firstname + " " + $scope.household.members[0].lastname; }, function (newVal, oldVal) {
-            if ($scope.household.postalname === oldVal || newVal === oldVal) {
-                $scope.household.postalname = newVal;
-            }
+                if ($scope.household.members[0].nickname === oldVal || newVal === oldVal) {
+                    $scope.household.members[0].nickname = $scope.household.members[0].firstname || "";
+                }
 
-            if ($scope.household.members[0].fullname === oldVal || newVal === oldVal) {
-                $scope.household.members[0].fullname = newVal;
-            }
-        });
-
-        $scope.$watch(function () { return parseInt($scope.costs.member) + parseInt($scope.costs.luncheon) }, function (newVal, oldVal) {
-            $scope.costs.total = newVal;
-        });
-    }])
+                if ($scope.household.members[0].fullname === oldVal || newVal === oldVal) {
+                    $scope.household.members[0].fullname = newVal || "";
+                }
+            });
+    }]);
+})(angular);
